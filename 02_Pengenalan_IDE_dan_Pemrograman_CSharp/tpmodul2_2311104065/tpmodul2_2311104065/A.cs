2b27622e72db49d63b4dd7e0c7be6db9fd7b0cac
rengganis <rengganistantri4@gmail.com>
2025-05-24 19:54:27 +0700

    tp modul 1

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/tpmodul2_2311104065/tpmodul2_2311104065/A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan satu huruf: ");
+        char huruf = Convert.ToChar(Console.ReadLine());
+
+        char[] vokal = { 'A', 'I', 'U', 'E', 'O' };
+
+        if (Array.Exists(vokal, element => element == huruf))
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf konsonan");
+        }
+    }
+}
