76ad4695064d30410f049ac3fff6e1dc68cc4214
rengganis <rengganistantri4@gmail.com>
2025-05-25 20:30:53 +0700

    menambahkan class HaloGeneric dengan method

--- 05_Generics/tpmodul5_2311104065/tpmodul5_2311104065/Program.cs
+++ 05_Generics/tpmodul5_2311104065/tpmodul5_2311104065/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul5_2311104065;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Rengganis"); 
+    }
+}
